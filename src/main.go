cd1a5430352ebe198ec22ca7c6eab19df5eda24f
clementreiffers <reiffersclement@gmail.com>
2023-12-19 14:37:51 +0100

    adding get route

--- src/main.go
+++ src/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
@@ -32,19 +30,44 @@ func filterUser(users []User, userName string) []User {
 
 func getUser(c *gin.Context) {
 	var searchUser User
+
+	userDatabase := c.MustGet("userDatabase").(*mongo.Collection)
 	if err := c.BindJSON(&searchUser); err != nil {
 		return
 	}
-	filteredUsers := filterUser(users, searchUser.Name)
-	c.IndentedJSON(http.StatusOK, filteredUsers)
+	//filter := bson.M{}
+
+	cursor, err := userDatabase.Find(context.TODO(), bson.D{{}})
+	//fmt.Printf("want to send: %+v\n", filteredUser)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var userFound []bson.M
+	if err = cursor.All(context.TODO(), &userFound); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, userFound)
 }
 
 func addUser(c *gin.Context) {
+	userDatabase := c.MustGet("userDatabase").(*mongo.Collection)
+
 	var newUser User
+
 	if err := c.BindJSON(&newUser); err != nil {
 		return
 	}
-	users = append(users, newUser)
+
+	//users = append(users, newUser)
+	insertResult, err := userDatabase.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("insert result : %s", insertResult)
 	c.IndentedJSON(http.StatusCreated, newUser)
 }
 
@@ -52,51 +75,56 @@ func deleteUser(c *gin.Context) {
 
 }
 
-func main() {
-	r := gin.Default()
-
-	r.GET("/ping", ping)
-
-	r.GET("/get-user", getUser)
-	r.POST("/add-user", addUser)
-	r.DELETE("/delete-User", deleteUser)
-
-	//err := r.Run()
-	//if err != nil {
-	//	fmt.Printf("%e\n", err)
-	//}
-
-	// make this follwing files connects to local mongodb
-
-	uri := "mongodb://localhost:27017/test"
-
+func connectMongo(uri string) *mongo.Client {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
+	return client
+}
 
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+func main() {
+	client := connectMongo("mongodb://localhost:27017/test")
+	userDatabase := client.Database("company").Collection("users")
 
-	coll := client.Database("sample_mflix").Collection("movies")
-	title := "Back to the Future"
+	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Set("userDatabase", userDatabase)
+		c.Next()
+	})
 
-	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
-	if errors.Is(err, mongo.ErrNoDocuments) {
-		fmt.Printf("No document was found with the title %s\n", title)
-		return
-	}
+	r.GET("/ping", ping)
+	r.GET("/get-user", getUser)
+	r.POST("/add-user", addUser)
+	r.DELETE("/delete-User", deleteUser)
+
+	err := r.Run()
 	if err != nil {
-		panic(err)
+		fmt.Printf("%e\n", err)
 	}
 
-	jsonData, err := json.MarshalIndent(result, "", "    ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s\n", jsonData)
+	//defer func() {
+	//	if err := client.Disconnect(context.TODO()); err != nil {
+	//		panic(err)
+	//	}
+	//}()
+	//
+	//coll := client.Database("sample_mflix").Collection("movies")
+	//title := "Back to the Future"
+	//
+	//var result bson.M
+	//err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	//if errors.Is(err, mongo.ErrNoDocuments) {
+	//	fmt.Printf("No document was found with the title %s\n", title)
+	//	return
+	//}
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//jsonData, err := json.MarshalIndent(result, "", "    ")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("%s\n", jsonData)
 }
